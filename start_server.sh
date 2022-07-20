bdc8d69fee776fc2405679070f1044da45cca78a
jerryan999 <jerryan1989@gmail.com>
2022-07-20 23:40:48 +0800

    shell

--- start_server.sh
+++ start_server.sh
@@ -1,12 +1,19 @@
-#/bin/bash
+#!/bin/bash
+
+# prepare log files and 
+touch logs/gunicorn-access.log
+touch logs/gunicorn.log
+
 # run celery worker in background
 supervisord -c /etc/supervisor/conf.d/supervisord.conf
 
-# run server in foreground
+
 gunicorn -b 0.0.0.0:5008 \
          --timeout 300 \
          --access-logfile logs/gunicorn-access.log \
-         --error-logfile - \
+         --error-logfile logs/gunicorn.log \
          --capture-output \
          --access-logformat '%(t)s %(p)s %(h)s "%r" %(s)s %(L)s %(b)s "%{Referer}i" "%{User-agent}i"' \
-         app:app
+         app:app \
+
+exec "$@"
