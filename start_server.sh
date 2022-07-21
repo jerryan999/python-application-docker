651901ea928fc2a9fd6a5c51dcbee08408e6a4ee
zhibin.an <zhibin.an@castbox.fm>
2022-07-21 14:02:37 +0800

    gunicorn log format

--- start_server.sh
+++ start_server.sh
@@ -13,7 +13,7 @@ gunicorn -b 0.0.0.0:5008 \
          --access-logfile logs/gunicorn-access.log \
          --error-logfile logs/gunicorn.log \
          --capture-output \
-         --access-logformat '%(t)s %(p)s %(h)s "%r" %(s)s %(L)s %(b)s "%{Referer}i" "%{User-agent}i"' \
+         --access-logformat "{'remote_ip':'%(h)s','request_id':'%({X-Request-Id}i)s','response_code':'%(s)s','request_method':'%(m)s','request_path':'%(U)s','request_querystring':'%(q)s','request_timetaken':'%(D)s','response_length':'%(B)s'}" \
          app:app \
 
 exec "$@"
